ad099e0749a6b8ea2b07a9bf6ffedad4e7c662a2
Doug Sauve <douglas_sauve@live.com>
2018-03-02 18:30:16 -0600

    Initial Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,44 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+var user = {name:"doug", age: 29};
+var {name} = user;
+console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+      return console.log(`unable to connect to MongoDB server: ${err}`);
+    }
+    console.log('Connected to MongoDB server.')
+
+    const db = client.db('TodoApp');
+    //todos, users - 2 collections in this db.
+
+
+    db.collection('Todos').insertOne({
+      text: 'eat broccoli',
+      completed: true
+    }, (err, result) => {
+      if (err) {
+        return console.log ('unable to insert to Todo.', err)
+      }
+
+      console.log(JSON.stringify(result.ops, undefined, 2)); //ops stores all of the docs that were inserted.
+
+    });
+
+    //Users: name, age, location.
+
+    // db.collection('Users').insertOne({
+    //   name: "Doug",
+    //   age: 25,
+    //   location: "Minnesota"
+    // }, (err, result) => {
+    //   if (err) console.log(err);
+    //
+    //   console.log(JSON.stringify(result.ops, undefined, 2));
+    //   console.log(result.ops[0]._id.getTimestamp());
+    // })
+
+
+    client.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,29 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+var user = {name:"doug", age: 29};
+var {name} = user;
+console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+      return console.log(`unable to connect to MongoDB server: ${err}`);
+    }
+    console.log('Connected to MongoDB server.')
+
+    const db = client.db('TodoApp');
+
+    db.collection('Todos')
+    .find({completed: true})
+    .toArray()
+    .then((docs) => {
+      console.log("Todos:");
+      console.log(JSON.stringify(docs, undefined, 2));
+
+    }, (err) => {
+      console.log("NO DICE MAN", err);
+    });
+
+
+
+//    client.close();
+});
