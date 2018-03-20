813cd911746b280fb7d2468c2dfc4528020d3a34
Doug Sauve <douglas_sauve@live.com>
2018-03-20 15:26:07 -0500

    add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -230,7 +230,7 @@ describe('POST users/me', () => {
         expect(user).toExist();
         expect(user.password).toNotBe(password);
         done();
-      })
+      }).catch((e) => done(e));
     });
   });
 
@@ -256,5 +256,45 @@ describe('POST users/me', () => {
       expect(res.body.email).toBe(email);
     })
     .end(done);
+  })
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token.', (done) => {
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email,
+      password: users[1].password
+    })
+    .expect(200)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toExist();
+    })
+    .end((err, res) => {
+      if (err) return done(err);
+
+      User.findById(users[1]._id).then((user) => {
+        expect(user.tokens[0]).toInclude({
+          access: 'auth',
+          token: res.headers['x-auth']
+        });
+        done();
+      }).catch((e) => done(e));
+    });
+  });
+
+  it('should reject invalid login information', (done) => {
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[0].email,
+      password: users[0].password + 1
+    })
+    .expect(400)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toNotExist();
+    })
+    .end(done());
   });
 });
