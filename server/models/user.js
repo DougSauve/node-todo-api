c95c0491dc3079d505f705658d8c56c10d2400b6
Doug Sauve <douglas_sauve@live.com>
2018-03-19 21:43:46 -0500

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -70,6 +70,21 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  const User = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user) return Promise.reject();
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, match) => {
+        if (!match) reject();
+        resolve(user);
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   const user = this;
 
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -102,7 +103,7 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 
-//POST users
+//POST users (sign up)
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
@@ -131,6 +132,17 @@ app.delete('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+//POST /users/login {email, password(in text) } //find user that matches email and hashed password.
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => res.status(400).send());
 });
 
 app.listen(port, () => {
